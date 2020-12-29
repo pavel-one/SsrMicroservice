56483af4ef3dcd25251ccb5f36018247d5f0ffc5
Pavel Zarubin <pavel@orendat.ru>
2020-12-29 12:57:48 +0300

    added live reload

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,11 @@
 const mix = require('laravel-mix');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+mix.webpackConfig({
+    plugins: [
+        new LiveReloadPlugin()
+    ]
+});
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/scss/app.scss', 'public/css');
